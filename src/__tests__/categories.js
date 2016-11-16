8c21ed3c94275e8e248af877b5f420be0121ebee
Nikita Gusakov <gusnkt@gmail.com>
2016-11-16 13:48:04 +0300

    test: add categories test

---
+++ src/__tests__/categories.js
@@ -0,0 +1,17 @@
+/**
+ * Copyright 2016 Dialog LLC <info@dlg.im>
+ * @flow
+ */
+
+import { categories, getEmojiByChar } from '../index';
+
+describe('categories', () => {
+  it('ensure any emoji from any category will be resolved', () => {
+    Object.keys(categories).forEach((name) => {
+      categories[name].forEach((char) => {
+        const emoji = getEmojiByChar(char);
+        expect(emoji).not.toBe(null);
+      });
+    });
+  });
+});
