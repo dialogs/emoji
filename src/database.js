e8deadb9edb0f79b2f3aea4b2d919e0b4d238462
Nikita Gusakov <gusnkt@gmail.com>
2017-01-09 18:45:51 +0300

    fix: should use image typo

--- src/database.js
+++ src/database.js
@@ -21,8 +21,8 @@ export function getEmojiByName(name: string): ?Emoji {
   return nameIndex[name];
 }
 
-const _isAppleEmojiSupports = isAppleEmojiSupports();
+const _shouldUseImage = !isAppleEmojiSupports();
 
 export function shouldUseImage(): boolean {
-  return _isAppleEmojiSupports;
+  return _shouldUseImage;
 }
